a9e73641a870c2033c86e7356eef189558a44faa
Sulavi <Richard.Arkusinski@wallawalla.edu>
2015-10-26 22:42:55 -0700

    Makes it work

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
 	$('#menu').menu();
-	$('h1').css('color', 'red');
+	$('#toolbar').buttonset();
+	
+	// function callback() {
+      // setTimeout(function() {
+        // $( "#effect:visible" ).removeAttr( "style" ).fadeOut();
+      // }, 1000 );
+    // };
+	
+	// $("#games").click(function(){
+		// $('#menu').show();
+		// return false;
+	// });
+	// $("#games").blur(function(){
+		// $('#menu').hide();
+	// });
+
+	$("#games").on({
+		click: function(){
+			$('#menu').show();
+			return false;
+		},
+		blur: function(){
+			$('#menu').hide();
+		}
+	});
 });
+
