42d58e2042ac0fda2dea095b411e367633a24b31
Sulavi <Richard.Arkusinski@wallawalla.edu>
2015-10-29 16:19:17 -0700

    Dunno What this did

--- script.js
+++ script.js
@@ -2,20 +2,6 @@ $(document).ready(function(){
 	$('#menu').menu();
 	$('#toolbar').buttonset();
 	
-	// function callback() {
-      // setTimeout(function() {
-        // $( "#effect:visible" ).removeAttr( "style" ).fadeOut();
-      // }, 1000 );
-    // };
-	
-	// $("#games").click(function(){
-		// $('#menu').show();
-		// return false;
-	// });
-	// $("#games").blur(function(){
-		// $('#menu').hide();
-	// });
-
 	$("#games").on({
 		click: function(){
 			$('#menu').show();
